6cea85503c91f52fea46ec6d1612b74f1ff4adc0
jvistorscosta <jvictorscosta@gmail.com>
2023-07-12 22:22:34 -0300

    Enviando codigo

---
+++ Conversor/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a temperatura em Fª ");
+        double entrada = scanner.nextDouble();
+
+        System.out.printf("%.2f Fª em Cº é igual a %.2f",entrada,converterTemp(entrada));
+        scanner.close();
+
+    }
+    public static double converterTemp(double fahrenheit){
+        double celsius = 5 * ((fahrenheit - 32) / 9);
+        return (celsius);
+    };
+
+}
